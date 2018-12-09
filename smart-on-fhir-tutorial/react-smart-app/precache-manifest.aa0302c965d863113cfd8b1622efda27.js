d8e78a24e19033924878c604c6351a0914e68597
dq qian <dqian@dqs-MacBook-Pro.local>
2018-12-09 10:29:43 -0700

    copy static files from compilation

---
+++ smart-on-fhir-tutorial/react-smart-app/precache-manifest.aa0302c965d863113cfd8b1622efda27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4341b017d22c8634d599",
+    "url": "/smart/mhn-container/static/js/runtime~main.4341b017.js"
+  },
+  {
+    "revision": "ea50e694fdbdc3ea31f8",
+    "url": "/smart/mhn-container/static/js/main.ea50e694.chunk.js"
+  },
+  {
+    "revision": "078d999e3ae12dd1192e",
+    "url": "/smart/mhn-container/static/js/1.078d999e.chunk.js"
+  },
+  {
+    "revision": "ea50e694fdbdc3ea31f8",
+    "url": "/smart/mhn-container/static/css/main.83012ea0.chunk.css"
+  },
+  {
+    "revision": "efc5e038f5e378f3298ef99cdc9781f1",
+    "url": "/smart/mhn-container/index.html"
+  }
+];
