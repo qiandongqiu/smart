5d785672125e1b3b66c9e0228144a500f0cf78dc
dq qian <dqian@dqs-MacBook-Pro.local>
2018-12-13 22:39:47 -0700

    dynamicly start apps

--- mhn-container/src/index.js
+++ mhn-container/src/index.js
@@ -2,4 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EncounterApp from './apps/encounter/encounter.js'
 
-ReactDOM.render(<EncounterApp />, document.getElementById("encounter"));
+
+class AppMain extends React.Component {
+    startApp(appName) {
+        switch(appName) {
+           case "encounter":
+                ReactDOM.render(<EncounterApp />, document.getElementById("encounter"));
+                break;
+           default:
+                console.log(appName + " is not defined in index.js!");
+        }
+    }
+
+    render() {
+        return (null);   
+    }
+}
+
+//DO NOT CHANGE ANYTHING FROM HERE DOWN
+window.mhnApplicationMain = ReactDOM.render(React.createElement(AppMain), document.getElementById("application_main"));
+
